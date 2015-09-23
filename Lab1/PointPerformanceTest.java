25419ebde42e45058a756102c3071ad5942a24a4
NikhilPeri <NikhilPeri@users.noreply.github.com>
2015-09-23 19:28:26 -0400

    Update PointPerformanceTest.java

--- Lab1/PointPerformanceTest.java
+++ Lab1/PointPerformanceTest.java
@@ -13,62 +13,62 @@ public class PointPerformanceTest {
 		PointP[] randomPointsP = new PointP[nPoints];
 		
 		//time to make n PontCP objects
-	    startTime = System.currentTimeMillis();
-	    for(int i = 0; i < nPoints; i++){
-	    	randomPointsCP[i] = new PointCP('C', random.nextDouble(), random.nextDouble());
-	    }
-	    stopTime = System.currentTimeMillis();
-	    elapsedTime = stopTime - startTime;
-	    System.out.println("The time to create "+ nPoints + " PointCP objects is: " + elapsedTime + " ms");
+	    	startTime = System.currentTimeMillis();
+	    	for(int i = 0; i < nPoints; i++){
+	    		randomPointsCP[i] = new PointCP('C', random.nextDouble(), random.nextDouble());
+	    	}
+	    	stopTime = System.currentTimeMillis();
+	    	elapsedTime = stopTime - startTime;
+	    	System.out.println("The time to create "+ nPoints + " PointCP objects is: " + elapsedTime + " ms");
 	    
-	    //time to make n PointP objects
-	    startTime = System.currentTimeMillis();
-	    for(int i = 0; i < nPoints; i++){
-	    	randomPointsP[i] = new PointP(random.nextDouble(), random.nextDouble());
-	    }
-	    stopTime = System.currentTimeMillis();
-	    elapsedTime = stopTime - startTime;
-	    System.out.println("The time to create "+ nPoints + " PointP objects is: " + elapsedTime + " ms\n");
+	    	//time to make n PointP objects
+	    	startTime = System.currentTimeMillis();
+	    	for(int i = 0; i < nPoints; i++){
+	    		randomPointsP[i] = new PointP(random.nextDouble(), random.nextDouble());
+	    	}
+	    	stopTime = System.currentTimeMillis();
+	    	elapsedTime = stopTime - startTime;
+	    	System.out.println("The time to create "+ nPoints + " PointP objects is: " + elapsedTime + " ms\n");
 	    
-	    //time to get n PointCP Cartesian Coordinates
-	    startTime = System.currentTimeMillis();
-	    for(int i = 0; i < nPoints; i++){
-	    	randomPointsCP[i].getX();
-	    	randomPointsCP[i].getY();
-	    }
-	    stopTime = System.currentTimeMillis();
-	    elapsedTime = stopTime - startTime;
-	    System.out.println("The time to get "+ nPoints + " cartesian coordinates (PointCP) is : " + elapsedTime + " ms");
+	    	//time to get n PointCP Cartesian Coordinates
+	    	startTime = System.currentTimeMillis();
+	    	for(int i = 0; i < nPoints; i++){
+	    		randomPointsCP[i].getX();
+	    		randomPointsCP[i].getY();
+		 }
+	    	stopTime = System.currentTimeMillis();
+	    	elapsedTime = stopTime - startTime;
+	    	System.out.println("The time to get "+ nPoints + " cartesian coordinates (PointCP) is : " + elapsedTime + " ms");
 	    
-	    //time to get n PointCP Polar Coordinates
-	    startTime = System.currentTimeMillis();
-	    for(int i = 0; i < nPoints; i++){
-	    	randomPointsCP[i].getRho();
-	    	randomPointsCP[i].getTheta();
-	    }
-	    stopTime = System.currentTimeMillis();
-	    elapsedTime = stopTime - startTime;
-	    System.out.println("The time to get "+ nPoints + " polar coordinates (PointCP) is : " + elapsedTime + " ms\n");
+		 //time to get n PointCP Polar Coordinates
+	    	startTime = System.currentTimeMillis();
+	    	for(int i = 0; i < nPoints; i++){
+	    		randomPointsCP[i].getRho();
+	    		randomPointsCP[i].getTheta();
+	    	}
+	    	stopTime = System.currentTimeMillis();
+	    	elapsedTime = stopTime - startTime;
+	    	System.out.println("The time to get "+ nPoints + " polar coordinates (PointCP) is : " + elapsedTime + " ms\n");
 	
-	//time to get n PointP Cartesian Coordinates
-    startTime = System.currentTimeMillis();
-    for(int i = 0; i < nPoints; i++){
-    	randomPointsP[i].getX();
-    	randomPointsP[i].getY();
-    }
-    stopTime = System.currentTimeMillis();
-    elapsedTime = stopTime - startTime;
-    System.out.println("The time to get "+ nPoints + " cartesian coordinates (PointP) is : " + elapsedTime + " ms");
+		//time to get n PointP Cartesian Coordinates
+		startTime = System.currentTimeMillis();
+		for(int i = 0; i < nPoints; i++){
+    			randomPointsP[i].getX();
+    			randomPointsP[i].getY();
+    		}
+    		stopTime = System.currentTimeMillis();
+    		elapsedTime = stopTime - startTime;
+    		System.out.println("The time to get "+ nPoints + " cartesian coordinates (PointP) is : " + elapsedTime + " ms");
     
-    //time to get n PointP Polar Coordinates
-    startTime = System.currentTimeMillis();
-    for(int i = 0; i < nPoints; i++){
-    	randomPointsP[i].getRho();
-    	randomPointsP[i].getTheta();
-    }
-    stopTime = System.currentTimeMillis();
-    elapsedTime = stopTime - startTime;
-    System.out.println("The time to get "+ nPoints + " polar coordinates (PointP) is : " + elapsedTime + " ms\n");
+		 //time to get n PointP Polar Coordinates
+	    	startTime = System.currentTimeMillis();
+    		for(int i = 0; i < nPoints; i++){
+    			randomPointsP[i].getRho();
+    			randomPointsP[i].getTheta();
+    		}
+    		stopTime = System.currentTimeMillis();
+    		elapsedTime = stopTime - startTime;
+    		System.out.println("The time to get "+ nPoints + " polar coordinates (PointP) is : " + elapsedTime + " ms\n");
     
-   }
+   	}
 }
